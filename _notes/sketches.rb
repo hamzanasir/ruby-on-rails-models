c0f4c1aeb5c4bb6ec6e8db7f9ae5e4822ff7307c
Hamza Nasir <mnasir@hawk.iit.edu>
2018-02-12 17:46:10 -0600

    Sudo code for models in sketches.rb

--- _notes/sketches.rb
+++ _notes/sketches.rb
@@ -2,3 +2,24 @@
 #imagined api
 
 @books = Book.all
+@books = Book.find(7)
+
+@book.title
+
+@book.author # one author
+@book.author.last_name # one author, last name
+
+# if one book can have many authors
+@book.authors.each do |author|
+  author.last_name
+end
+
+# work with Authors
+
+@author = Author.find(4)
+
+@author.books.count # number of books?
+
+@author.books.each do |book|
+  book.title
+end
