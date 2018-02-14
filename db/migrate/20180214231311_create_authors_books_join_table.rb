5f62901e103c08576553b0300a075d08136172cc
Hamza Nasir <mnasir@hawk.iit.edu>
2018-02-14 17:27:45 -0600

    Run authors book migration

--- db/migrate/20180214231311_create_authors_books_join_table.rb
+++ db/migrate/20180214231311_create_authors_books_join_table.rb
@@ -3,5 +3,6 @@ class CreateAuthorsBooksJoinTable < ActiveRecord::Migration[5.1]
     create_table :authors_books, id: false do |t|
       t.belongs_to :author, index: true
       t.belongs_to :book, index: true
+    end
   end
 end
