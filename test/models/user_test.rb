74da3be2724afe532f116dc439f4ff8d0702ca32
Hamza Nasir <mnasir@hawk.iit.edu>
2018-02-21 17:57:57 -0600

    Ensured that user without email doesn't get saved

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user email address must be unique" do
+    user = User.new
+    user.role = roles(:user)
+    user.email = users(:scarlet).email
+    assert_not user.save, "Saved a duplicate email address"
+  end
 end
